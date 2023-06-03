63a9f9844380484832c350f47d6314b3961a9217
David Galeno <davidgaleno411@gmail.com>
2023-06-03 17:01:07 -0300

    layout-conversor-numeros

--- src/components/ConversorNumeros/index.tsx
+++ src/components/ConversorNumeros/index.tsx
@@ -1,8 +1,15 @@
 import styles from './styles.module.css'
 export const ConversorNumero = () => {
-    return(
+    return (
         <div className={styles.container}>
-            
+            <form className={styles.conversorContainer}>
+                <h1 className={styles.title}>Conversor Entre Números Arábicos e Romanos</h1>
+                <div className={styles.numbers}>
+                    <input type="number" className={styles.numeroArabico} name="numeroArabico" id="numeroArabico" placeholder='Digite um número arábico' />
+                    <input type="text" className={styles.numeroArabico} name="numeroArabico" id="numeroArabico" placeholder='Digite um número arábico' />
+                </div>
+                <button className={styles.conversor}>Converter</button>
+            </form>
         </div>
     )
 }

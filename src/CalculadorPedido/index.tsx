8178299e942eeef1b1845bd3d731691d54480c0e
David Galeno <davidgaleno411@gmail.com>
2023-06-04 12:22:36 -0300

    finish-price-division-logic

--- src/CalculadorPedido/index.tsx
+++ src/CalculadorPedido/index.tsx
@@ -10,21 +10,28 @@ const valorProdutos: Record<string, number> = {
 
 const consumos: Record<string, string[]> = {
     David: ['Refrigerante', 'Peixe'],
-    Debora: ['Refrigerante', 'Salada','Peixe'],
+    Debora: ['Refrigerante', 'Salada', 'Peixe'],
     Julio: ['Salada', 'Cerveja']
 }
 
 
 
 export const CalculadoraPedido = () => {
-    const [clientes, setClientes] = useState(['David', 'Debora','Julio'])
-    const [produtos, setProdutos] = useState(['Refrigerante', 'Peixe','Salada','Cerveja'])
+    const [clientes, setClientes] = useState(['David', 'Debora', 'Julio'])
+    const [produtos, setProdutos] = useState(['Refrigerante', 'Peixe', 'Salada', 'Cerveja'])
 
     const produtosDivididos = ['Salada']
 
-
-
     const calcularValor = () => {
+
+
+        const pagamentoTaxa: Record<string, boolean> = {
+            David: false,
+            Debora: false,
+            Julio: false
+        }
+
+
         const valorConsumo: Record<string, number> = {
             David: 0,
             Debora: 0,
@@ -34,8 +41,8 @@ export const CalculadoraPedido = () => {
         const ocorrenciasProduto: Record<string, number> = {
             Refrigerante: 0,
             Peixe: 0,
-            Salada:0,
-            Cerveja:0
+            Salada: 0,
+            Cerveja: 0
         }
 
         for (let values of Object.values(consumos)) {
@@ -45,7 +52,6 @@ export const CalculadoraPedido = () => {
                 }
             }
         }
-        console.log(ocorrenciasProduto)
 
 
         for (let [key, values] of Object.entries(consumos)) {
@@ -53,7 +59,6 @@ export const CalculadoraPedido = () => {
                 if (values.includes(produto)) {
                     if (produtosDivididos.includes(produto)) {
                         valorConsumo[key] += valorProdutos[produto] / ocorrenciasProduto[produto]
-
                     }
                     else {
                         valorConsumo[key] += valorProdutos[produto]
@@ -69,11 +74,26 @@ export const CalculadoraPedido = () => {
 
         for (let [key, values] of Object.entries(consumos)) {
             for (let produto of produtosUnicos) {
-                if (values.includes(produto))
+                if (values.includes(produto)) {
                     valorConsumo[key] += valorConsumo[key] * 0.1
+                    pagamentoTaxa[key] = true
+                }
+            }
+
+        }
+        for (let [produto, ocorrencia] of Object.entries(ocorrenciasProduto)) {
+
+            if (ocorrencia > 1 && produtosDivididos.includes(produto) === false) {
+                for (let [pessoa, produtos] of Object.entries(consumos)) {
+                    if (produtos.includes(produto) && !pagamentoTaxa[pessoa]) {
+                        valorConsumo[pessoa] += valorConsumo[pessoa] * 0.1
+                        pagamentoTaxa[pessoa] = true
+                    }
+                }
+
             }
         }
-        console.log(valorConsumo)
+
 
     }
 

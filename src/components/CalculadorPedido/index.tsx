8f7c23ba4430927a120013d63f24e55d8ab6863e
David Galeno <davidgaleno411@gmail.com>
2023-06-05 00:05:21 -0300

    dividir-produto-implemented

--- src/components/CalculadorPedido/index.tsx
+++ src/components/CalculadorPedido/index.tsx
@@ -14,94 +14,111 @@ const consumos: Record<string, string[]> = {
     Julio: ['Salada', 'Cerveja']
 }
 
-
+interface Produto {
+    nome: string
+    preco: number
+    divisao: string[]
+}
+interface Compra {
+    cliente: string
+    produto: string[]
+}
 
 export const CalculadoraPedido = () => {
-    const [clientes, setClientes] = useState(['David', 'Debora', 'Julio'])
-    const [produtos, setProdutos] = useState(['Refrigerante', 'Peixe', 'Salada', 'Cerveja'])
-
-    const produtosDivididos = ['Salada']
-
-
-
-    const calcularValor = () => {
-
-        const pagamentoTaxa: Record<string, boolean> = {
-            David: false,
-            Debora: false,
-            Julio: false
+    const taxaServico = 0.1
+    const [produto, setProduto] = useState<string>()
+    const [produtoDividido, setProdutoDividido] = useState<string>()
+    const [nome, setNome] = useState<string>('')
+    const [clientes, setClientes] = useState<string[]>([])
+    const [compras, setCompras] = useState<Record<string, string[]>>({})
+    const [valorConsumo, setValorConsumo] = useState<Record<string, number>>({})
+    const [produtos, setProdutos] = useState<Produto[]>([
+        {
+            nome: 'Refrigerante', preco: 8, divisao: []
+        },
+        {
+            nome: 'Peixe', preco: 20, divisao: []
+        },
+        {
+            nome: 'Salada', preco: 4, divisao: []
+        },
+        {
+            nome: 'Cerveja', preco: 6, divisao: []
         }
+    ]);
 
+    const criarPedido = () => {
 
-        const valorConsumo: Record<string, number> = {
-            David: 0,
-            Debora: 0,
-            Julio: 0
-        }
+        if (!valorConsumo[nome]) setClientes(prevClientes => [...prevClientes, nome])
+        const produtoComprado = produtos.filter((item: Produto) => item.nome === produto)
 
-        const ocorrenciasProduto: Record<string, number> = {
-            Refrigerante: 0,
-            Peixe: 0,
-            Salada: 0,
-            Cerveja: 0
-        }
-
-        for (let values of Object.values(consumos)) {
-            for (let produto of produtos) {
-                if (values.includes(produto)) {
-                    ocorrenciasProduto[produto] += 1
-                }
-            }
-        }
-
-
-        for (let [key, values] of Object.entries(consumos)) {
-            for (let produto of produtos) {
-                if (values.includes(produto)) {
-                    if (produtosDivididos.includes(produto)) {
-                        valorConsumo[key] += valorProdutos[produto] / ocorrenciasProduto[produto]
-                    }
-                    else {
-                        valorConsumo[key] += valorProdutos[produto]
-                    }
-                }
-            }
-        }
-
-        const produtosUnicos = []
-        for (let produto of Object.keys(ocorrenciasProduto)) {
-            if (ocorrenciasProduto[produto] === 1) produtosUnicos.push(produto)
-        }
-
-        for (let [key, values] of Object.entries(consumos)) {
-            for (let produto of produtosUnicos) {
-                if (values.includes(produto)) {
-                    valorConsumo[key] += valorConsumo[key] * 0.1
-                    pagamentoTaxa[key] = true
-                }
-            }
-
-        }
-        for (let [produto, ocorrencia] of Object.entries(ocorrenciasProduto)) {
-
-            if (ocorrencia > 1 && produtosDivididos.includes(produto) === false) {
-                for (let [pessoa, produtos] of Object.entries(consumos)) {
-                    if (produtos.includes(produto) && !pagamentoTaxa[pessoa]) {
-                        valorConsumo[pessoa] += valorConsumo[pessoa] * 0.1
-                        pagamentoTaxa[pessoa] = true
-                    }
-                }
-
-            }
-        }
+        const copiaValorConsumo = { ...valorConsumo }
+        if (!copiaValorConsumo[nome]) copiaValorConsumo[nome] = 0
+        copiaValorConsumo[nome] += produtoComprado[0].preco
+        setValorConsumo(copiaValorConsumo)
     }
 
-    console.log()
+    const dividirProduto = () => {
+        if (!valorConsumo[nome]) return;
+        const produtoComprado = produtos.find((item: Produto) => item.nome === produto);
+
+        if (produtoComprado) {
+            const updatedProdutos = [...produtos]; // Create a copy of the produtos array
+            const index = updatedProdutos.indexOf(produtoComprado);
+            updatedProdutos[index] = {
+                ...updatedProdutos[index], // Copy the existing product object
+                divisao: [...updatedProdutos[index].divisao, nome] // Update the divisao array
+            };
+            setProdutos(updatedProdutos); // Update the state with the modified array
+        }
+    };
+    const finalizarPedido = () => {
+
+        produtos.forEach((produto: Produto) => {
+            if (produto.divisao.length < 1) return
+            const copiaValorConsumo = { ...valorConsumo };
+            const quantidadeDivisao = produto.divisao.length;
+            produto.divisao.forEach(cliente => {
+                copiaValorConsumo[cliente] -= produto.preco / quantidadeDivisao;
+            });
+            console.log(copiaValorConsumo)
+            clientes.forEach(cliente => {
+                copiaValorConsumo[cliente] += Number((copiaValorConsumo[cliente] * 0.1).toFixed(2))
+
+            })
+            setValorConsumo(prevValorConsumo => ({ ...prevValorConsumo, ...copiaValorConsumo }));
+            console.log(produtos)
+        });
+
+    };
+
+
     return (
-        <div className="container">
-            <button onClick={() => {
-                calcularValor()
-            }}>Enviar</button>
+        <div className={styles.container}>
+            <div className={styles.clientes}>
+                {clientes.map((cliente: string, index: number) => (
+                    <div className={styles.clienteContainer}>
+                        <span key={index} className={styles.cliente}>{cliente}</span>
+                        {valorConsumo[cliente] && <span key={index} className={styles.cliente}>R${valorConsumo[cliente]}</span>}
+                    </div>
+                ))
+                }
+
+            </div>
+            <form onSubmit={(e) => e.preventDefault()} className={styles.cadastro}>
+                <input className={styles.input} type="text" onChange={(e) => setNome(e.target.value)} value={nome} name="" id="" placeholder='Digite o nome do cliente' required />
+                <select className={styles.produtos} onChange={(e) => setProduto(e.target.value)} value={produto} name="" id="" required>
+                    <option value="" hidden selected>Selecione um produto para ser comprado</option>
+                    {produtos.map((produto: Produto) => <option className={styles.produto}>{produto.nome}</option>)}
+                </select>
+
+                <div className={styles.buttons}>
+                    <button className={styles.button} onClick={(e) => dividirProduto()}>Dividir Produto</button>
+
+                    <button className={styles.button} onClick={(e) => criarPedido()}>Criar Pedido</button>
+                    <button className={styles.button} onClick={(e) => finalizarPedido()}>Finalizar Pedido</button>
+                </div>
+            </form>
         </div>
     )
 }

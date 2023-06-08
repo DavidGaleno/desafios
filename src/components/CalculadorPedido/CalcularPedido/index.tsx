473d9ad4e04115c90291ba5d11fb51215a864d3a
David Galeno <davidgaleno411@gmail.com>
2023-06-07 23:12:09 -0300

    feat-refactor-and-fix-bugs

--- src/components/CalculadorPedido/CalcularPedido/index.tsx
+++ src/components/CalculadorPedido/CalcularPedido/index.tsx
@@ -11,12 +11,16 @@ import { ButtonHome } from '../../../Home/ButtonHome'
 
 export const CalculadoraPedido = () => {
     const taxaServico = 0.1
-    const [textDividirProduto, setTextDividirProduto] = useState<string>('Dividir Produto')
-    const [valorTotal, setValorTotal] = useState<Record<string, number>[]>([])
-    const [produto, setProduto] = useState<string>()
-    const [nome, setNome] = useState<string>('')
-    const [clientes, setClientes] = useState<string[]>([])
+    //Representa o pago inicialmente por cada usuário, sem considerar taxas e divisões
     const [valorConsumo, setValorConsumo] = useState<Record<string, number>>({})
+    //Valor resultado da soma dos produtos do valorConsumo de cada cliente + taxa de serviço - descontos de produtos divididos
+    const [valorFinal, setvalorFinal] = useState<Record<string, number>[]>([])
+    //Esse texto será Dividir Produto quando o usuário não tiver clicado no botão dividir produto e será Não Dividir Produto quando o
+    //usuário já tiver clicado em Dividir Produto
+    const [textDividirProduto, setTextDividirProduto] = useState<string>('Dividir Produto')
+    const [produto, setProduto] = useState<string>()
+    const [usuario, setUsuario] = useState<string>('')
+    const [clientes, setClientes] = useState<string[]>([])
     const [compras, setCompras] = useState<Record<string, string[]>[]>([])
     const [produtos, setProdutos] = useState<IProduto[]>([
         {
@@ -32,36 +36,48 @@ export const CalculadoraPedido = () => {
             nome: 'Cerveja', preco: 6, divisao: []
         }
     ]);
+
+    //Sempre que o usuário no Input, o produto no Select ou os usuarios inseridos dentro do produto.divisao mudar, será checado
+    //se o usuário já pediu para dividir esse produto, podendo mudar a aparência do textDividirProduto 
     useEffect(() => {
         const produtoComprado = produtos.find((item: IProduto) => item.nome === produto);
-        if (produtoComprado?.divisao.includes(nome)) return setTextDividirProduto('Não Dividir Produto')
+        if (produtoComprado?.divisao.includes(usuario)) return setTextDividirProduto('Não Dividir Produto')
         return setTextDividirProduto('Dividir Produto')
-    }, [nome, produto, produtos])
+    }, [usuario, produto, produtos])
 
     const criarPedido = () => {
-        setValorTotal([])
-        if (!produto || !nome) return
-        if (!valorConsumo[nome]) setClientes(prevClientes => [...prevClientes, nome])
+        //Reinicia o valor final da aplicação. Só faz sentido após a finalização de um pedido
+        setvalorFinal([])
+
+        if (!produto || !usuario) return
+        if (!valorConsumo[usuario]) setClientes(prevClientes => [...prevClientes, usuario])
         const produtoComprado = produtos.filter((item: IProduto) => item.nome === produto)
 
+        //Cria uma cópia do array que representa o valor consumido por cada cliente
+        //Será somado o valor de cada produto comprado pelo cliente
         const copiaValorConsumo = { ...valorConsumo }
-        if (!copiaValorConsumo[nome]) copiaValorConsumo[nome] = 0
-        copiaValorConsumo[nome] += produtoComprado[0].preco
+        if (!copiaValorConsumo[usuario]) copiaValorConsumo[usuario] = 0
+        copiaValorConsumo[usuario] += produtoComprado[0].preco
         setValorConsumo(copiaValorConsumo)
-        const compra: Record<string, string[]> = {}
-        compra[nome] = []
-        compra[nome].push(produto)
 
+
+        const compra: Record<string, string[]> = {}
+        compra[usuario] = []
+        compra[usuario].push(produto)
+
+        //É criada uma cópia do array com as compras dos usuários
+        //É criada uma variável item que representa um usuário e suas compras
+        //Se já houver um item com o usuário escrito no Input, será apenas adicionado um usuário ao array
         const updatedCompras = [...compras]
         for (let i = 0; i < updatedCompras.length; i++) {
             const item = updatedCompras[i]
-            if (item[nome]) {
-                updatedCompras[i] = { ...item, [nome]: [...item[nome], produto] }
+            if (item[usuario]) {
+                updatedCompras[i] = { ...item, [usuario]: [...item[usuario], produto] }
                 setCompras(updatedCompras)
                 return
             }
         }
-
+        //Se o usuário ainda não estiver no array será adicionado
         updatedCompras.push(compra)
         setCompras(updatedCompras)
 
@@ -69,22 +85,25 @@ export const CalculadoraPedido = () => {
 
 
     const dividirProduto = () => {
-        if (!valorConsumo[nome] || !produto || !nome) return;
-        
+        if (!valorConsumo[usuario] || !produto || !usuario) return;
+
 
         const produtoComprado = produtos.find((item: IProduto) => item.nome === produto);
 
+        //Se o produto já tiver sido comprado, então será autorizada a divisão do produto
         if (produtoComprado) {
             const updatedProdutos = [...produtos];
             const index = updatedProdutos.indexOf(produtoComprado);
-            if (produtoComprado.divisao.includes(nome)) {
-                produtoComprado.divisao.splice(produtoComprado.divisao.indexOf(nome), 1)
-                updatedProdutos[index] = { ...updatedProdutos[index], [nome]: produtoComprado }
+            //Caso o produto já tenha sido dividido, será cancelada a divisão
+            if (produtoComprado.divisao.includes(usuario)) {
+                produtoComprado.divisao.splice(produtoComprado.divisao.indexOf(usuario), 1)
+                updatedProdutos[index] = { ...updatedProdutos[index], [usuario]: produtoComprado }
             }
+            //O produto será dividido
             else {
                 updatedProdutos[index] = {
                     ...updatedProdutos[index],
-                    divisao: [...updatedProdutos[index].divisao, nome]
+                    divisao: [...updatedProdutos[index].divisao, usuario]
                 };
             }
             setProdutos(updatedProdutos);
@@ -93,8 +112,7 @@ export const CalculadoraPedido = () => {
     const finalizarPedido = () => {
         const copiaValorConsumo = { ...valorConsumo };
         produtos.forEach((produto: IProduto) => {
-
-
+            //Serão realizados os descontos devido a divisão dos produtos
             produto.divisao.forEach(cliente => {
                 if (produto.divisao.length > 1) {
                     const quantidadeDivisao = produto.divisao.length;
@@ -102,15 +120,17 @@ export const CalculadoraPedido = () => {
                 }
             });
         });
-        const updatedValorTotal = [...valorTotal]
+        const updatedvalorFinal = [...valorFinal]
+        //Realiza a aplicação das taxas de serviço no valor final
         clientes.forEach(cliente => {
             copiaValorConsumo[cliente] += Number((copiaValorConsumo[cliente] * taxaServico).toFixed(2))
-            updatedValorTotal.push({ [cliente]: copiaValorConsumo[cliente] })
+            updatedvalorFinal.push({ [cliente]: copiaValorConsumo[cliente] })
         })
+        //Zera os valores de todas as variáveis, salvo a variável valorFinal
         setValorConsumo({})
         setClientes([])
-        setValorTotal(updatedValorTotal)
-        setNome('')
+        setvalorFinal(updatedvalorFinal)
+        setUsuario('')
         setProduto("")
         const copiaProdutos = produtos
         copiaProdutos.forEach(produto => produto.divisao = [])
@@ -118,17 +138,18 @@ export const CalculadoraPedido = () => {
         setCompras([])
 
     };
+        
     const deletarPedido = (_e: MouseEvent<HTMLSpanElement>, cliente: string, produtoComprado: string) => {
 
         const updatedCompras = [...compras]
         for (const compra of updatedCompras) {
-            if (compra[cliente].includes(produtoComprado)) compra[cliente].splice(compra[cliente].indexOf(produtoComprado), 1)
+            if (compra[cliente] && compra[cliente].includes(produtoComprado)) compra[cliente].splice(compra[cliente].indexOf(produtoComprado), 1)
             setCompras(updatedCompras)
 
             const copiaValorConsumo = { ...valorConsumo }
             const produtoRemovido = produtos.find(produto => produto.nome === produtoComprado)
             if (produtoRemovido) {
-                copiaValorConsumo[nome] -= produtoRemovido.preco
+                copiaValorConsumo[usuario] -= produtoRemovido.preco
                 setValorConsumo(copiaValorConsumo)
             }
 
@@ -156,8 +177,8 @@ export const CalculadoraPedido = () => {
     const limpar = () => {
         setValorConsumo({})
         setClientes([])
-        setValorTotal([])
-        setNome('')
+        setvalorFinal([])
+        setUsuario('')
         setProduto("")
         setCompras([])
     }
@@ -173,11 +194,11 @@ export const CalculadoraPedido = () => {
                 <Button onClick={() => limpar()} text='Limpar' />
             </div>
             <form onSubmit={(e) => e.preventDefault()} className={styles.cadastro}>
-                <Input maxLength={28} onChange={(e) => setNome(e.target.value)} value={nome} placeholder='Digite o nome do cliente' />
+                <Input maxLength={28} onChange={(e) => setUsuario(e.target.value)} value={usuario} placeholder='Digite o nome do cliente' />
                 <Select options={produtos} value={produto} onChange={(e) => setProduto(e.target.value)} />
             </form>
             <div className={styles.clientes}>
-                {valorTotal.length > 0 && valorTotal.map(valor => (<Span text={`${Object.keys(valor)[0]}: R$${Object.values(valor)[0]}`} />
+                {valorFinal.length > 0 && valorFinal.map(valor => (<Span text={`${Object.keys(valor)[0]}: R$${Object.values(valor)[0]}`} />
                 ))}
                 {compras.map((compra: Record<string, string[]>, index: number) => (
                     <div className={styles.clienteContainer}>
